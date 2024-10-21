74fd9f0625934ab991e593c75d7d492cc725e47e
Valeria <valeria.becerra@unach.udu.ec>
2024-10-21 16:57:08 -0500

    actualización del index con los estilos

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Imagen from '../assets/loginvector.png'
 
 const Login = () =>{
     return(
-        <div>
-            <h2>ESTE ES EL LOGIN</h2>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-4'>
+                    <div className='padre'>
+                        <div className='card card-body'>
+                            <form action="">
+                                <input type="text" placeholder='Ingresar mail' />
+                                <input type="text" placeholder='Ingresar contraseña' />
+                                <button>Registrarse</button>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+                <div className='col-md-8'>
+                    <img src={Imagen} alt="" className='tamaño-imagen' />
+                </div>
+            </div>
         </div>
     )
 }
